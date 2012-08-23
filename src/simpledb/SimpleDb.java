a88af840ce9a93aff0142af1784e09ec5f0a84d0
Mason Chang <mason.chang@oracle.com>
2012-08-23 09:53:13 -0700

    Refactored parse args from main

--- src/simpledb/SimpleDb.java
+++ src/simpledb/SimpleDb.java
@@ -511,19 +511,85 @@ public class SimpleDb {
     	}
     }
     
-    public static void main (String args[])
-            throws DbException, TransactionAbortedException, IOException {
-
-        if (args.length == 0) {
-            customTests();
-            System.out.println("Did custom tests");
-            System.exit(1);
+    private boolean convertArgs(String arg) {
+    	return arg.equalsIgnoreCase("convert");
+    }
+    
+    private boolean print(String arg) {
+    	return arg.equalsIgnoreCase("print");
+    }
+    
+    private boolean invokeParser(String arg) {
+    	return arg.equalsIgnoreCase("parser");
+    }
+    
+    public void parseArgs(String[] args) throws Exception, TransactionAbortedException {
+    	int argLength = args.length;
+    	assert (argLength > 0);
+    	String command = args[0];
+    	
+        if (convertArgs(command)) {
+        	convertFile(args);
+        } else if (print(command)) {
+            printTable(args);
+        } else if (invokeParser(command)) {
+            parseStatement(args);
+        } else {
+        	usage(command);
         }
-        
-        // convert a file
-        if(args[0].equals("convert")) {
-        try {
-        if (args.length == 3) {
+    }
+
+	private void usage(String command) {
+		System.err.println("Unknown command: " + command);
+		System.err.println("Valid commands are: convert, print, parser");
+        System.exit(1);
+	}
+
+	private void parseStatement(String[] args) {
+		// Strip the first argument and call the parser
+		String[] newargs = new String[args.length-1];
+		for (int i = 1; i < args.length; ++i) {
+		    newargs[i-1] = args[i];
+		}
+		
+		try {
+		    //dynamically load Parser -- if it doesn't exist, print error message
+		    Class<?> c = Class.forName("simpledb.Parser");
+		    Class<?> s = String[].class;
+		    
+		    java.lang.reflect.Method m = c.getMethod("main", s);
+		    m.invoke(null, (java.lang.Object)newargs);
+		} catch (ClassNotFoundException cne) {
+		    System.out.println("Class Parser not found -- perhaps you are trying to run the parser as a part of lab1?");
+		}
+		catch (Exception e) {
+		    System.out.println("Error in parser.");
+		    e.printStackTrace();
+		}
+	}
+
+	private void printTable(String[] args) throws DbException,
+			TransactionAbortedException {
+		File tableFile = new File(args[1]);
+		int columns = Integer.parseInt(args[2]);
+		DbFile table = Utility.openHeapFile(columns, tableFile);
+		TransactionId tid = new TransactionId();
+		DbFileIterator it = table.iterator(tid);
+		
+		if(null == it){
+		   System.out.println("Error: method HeapFile.iterator(TransactionId tid) not yet implemented!");
+		} else {
+		   it.open();
+		   while (it.hasNext()) {
+		      Tuple t = it.next();
+		      System.out.println(t);
+		   }
+		   it.close();
+		}
+	}
+
+	private void convertFile(String[] args) throws IOException {
+		if (args.length == 3) {
             HeapFileEncoder.convert(new File(args[1]),
                         new File(args[1].replaceAll(".txt", ".dat")),
                         BufferPool.PAGE_SIZE,
@@ -550,54 +616,16 @@ public class SimpleDb {
         } else {
             System.out.println("Unexpected number of arguments to convert ");
         }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        } else if (args[0].equals("print")) {
-            File tableFile = new File(args[1]);
-            int columns = Integer.parseInt(args[2]);
-            DbFile table = Utility.openHeapFile(columns, tableFile);
-            TransactionId tid = new TransactionId();
-            DbFileIterator it = table.iterator(tid);
-            
-            if(null == it){
-               System.out.println("Error: method HeapFile.iterator(TransactionId tid) not yet implemented!");
-            } else {
-               it.open();
-               while (it.hasNext()) {
-                  Tuple t = it.next();
-                  System.out.println(t);
-               }
-               it.close();
-            }
-        }
-        else if (args[0].equals("parser")) {
-            // Strip the first argument and call the parser
-            String[] newargs = new String[args.length-1];
-            for (int i = 1; i < args.length; ++i) {
-                newargs[i-1] = args[i];
-            }
-            
-            try {
-                //dynamically load Parser -- if it doesn't exist, print error message
-                Class<?> c = Class.forName("simpledb.Parser");
-                Class<?> s = String[].class;
-                
-                java.lang.reflect.Method m = c.getMethod("main", s);
-                m.invoke(null, (java.lang.Object)newargs);
-            } catch (ClassNotFoundException cne) {
-                System.out.println("Class Parser not found -- perhaps you are trying to run the parser as a part of lab1?");
-            }
-            catch (Exception e) {
-                System.out.println("Error in parser.");
-                e.printStackTrace();
-            }
+	}
+    
+    public static void main (String args[])
+            throws DbException, TransactionAbortedException, IOException, Exception {
 
-        }
-        else {
-            System.err.println("Unknown command: " + args[0]);
-            System.exit(1);
+        if (args.length == 0) {
+            customTests();
+        } else {
+        	SimpleDb database = new SimpleDb();
+	        database.parseArgs(args);
         }
     }
-
 }
